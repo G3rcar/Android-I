bf759d945a7063a4171e02d3678d65d5b4fd6b98
Gerardo Calderón <estudinteudb-cep@iMac03.local>
2013-10-23 08:08:39 -0600

    Fin Clase 1

--- Clase02CicloDeVida/gen/com/udb/modulo1/clase02/R.java
+++ Clase02CicloDeVida/gen/com/udb/modulo1/clase02/R.java
@@ -26,8 +26,8 @@ public final class R {
     public static final class id {
         public static final int ScrollView1=0x7f080000;
         public static final int action_settings=0x7f080003;
-        public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080002;
+        public static final int txvCiclo=0x7f080001;
+        public static final int txvHola=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -39,6 +39,8 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int mensaje=0x7f050003;
+        public static final int mensaje1=0x7f050004;
     }
     public static final class style {
         /** 
