522ce7053057d1f6e6dc2f816e4919cbcccc5c6d
Gerardo Calderón <estudinteudb-cep@iMac03.local>
2013-10-25 08:10:56 -0600

    Fin de la clase 2

--- Clase04Mantenimiento/gen/com/udb/modulo1/clase04/R.java
+++ Clase04Mantenimiento/gen/com/udb/modulo1/clase04/R.java
@@ -8,6 +8,9 @@
 package com.udb.modulo1.clase04;
 
 public final class R {
+    public static final class array {
+        public static final int arrDepartamentos=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -21,22 +24,40 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_enviar=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int ScrollView1=0x7f080000;
-        public static final int action_settings=0x7f080001;
+        public static final int ScrollView1=0x7f090000;
+        public static final int aedtDeptos=0x7f09000a;
+        public static final int button1=0x7f09000b;
+        public static final int edtEdad=0x7f090008;
+        public static final int edtEmail=0x7f090006;
+        public static final int edtPrimerApellido=0x7f090004;
+        public static final int edtPrimerNombre=0x7f090002;
+        public static final int itmEnviar=0x7f09000c;
+        public static final int textView1=0x7f090001;
+        public static final int textView2=0x7f090003;
+        public static final int textView3=0x7f090005;
+        public static final int textView4=0x7f090007;
+        public static final int textView5=0x7f090009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int btnEnviar=0x7f050009;
+        public static final int btnGuardar=0x7f050008;
         public static final int hello_world=0x7f050002;
+        public static final int lblCorreo=0x7f050005;
+        public static final int lblDepartamento=0x7f050007;
+        public static final int lblEdad=0x7f050006;
+        public static final int lblPrimerApellido=0x7f050004;
         public static final int lblPrimerNombre=0x7f050003;
     }
     public static final class style {
@@ -61,10 +82,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
