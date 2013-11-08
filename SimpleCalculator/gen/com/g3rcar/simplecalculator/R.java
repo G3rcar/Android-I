91004ec1fa5dcd48d8cb848e552080b584bc6786
Gerardo Calderon <gerardo.calderon@edge.com.sv>
2013-11-08 17:36:16 -0600

    Fin practica con calculadora

--- SimpleCalculator/gen/com/g3rcar/simplecalculator/R.java
+++ SimpleCalculator/gen/com/g3rcar/simplecalculator/R.java
@@ -19,27 +19,32 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int lblResultado=0x7f040002;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int border=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int ScrollView1=0x7f080000;
-        public static final int action_settings=0x7f080011;
-        public static final int btnCero=0x7f08000f;
-        public static final int btnCinco=0x7f080008;
-        public static final int btnClear=0x7f080010;
-        public static final int btnCuatro=0x7f080007;
-        public static final int btnDos=0x7f08000c;
-        public static final int btnIgual=0x7f08000e;
-        public static final int btnMas=0x7f080006;
-        public static final int btnMenos=0x7f08000a;
-        public static final int btnNueve=0x7f080005;
-        public static final int btnOcho=0x7f080004;
-        public static final int btnSeis=0x7f080009;
-        public static final int btnSiete=0x7f080003;
-        public static final int btnTres=0x7f08000d;
-        public static final int btnUno=0x7f08000b;
+        public static final int action_settings=0x7f080014;
+        public static final int btnCero=0x7f080010;
+        public static final int btnCinco=0x7f080009;
+        public static final int btnCuatro=0x7f080008;
+        public static final int btnDivision=0x7f080013;
+        public static final int btnDos=0x7f08000d;
+        public static final int btnIgual=0x7f080012;
+        public static final int btnLimpiar=0x7f080003;
+        public static final int btnMas=0x7f080007;
+        public static final int btnMenos=0x7f08000b;
+        public static final int btnMultiplicacion=0x7f08000f;
+        public static final int btnNueve=0x7f080006;
+        public static final int btnOcho=0x7f080005;
+        public static final int btnPunto=0x7f080011;
+        public static final int btnSeis=0x7f08000a;
+        public static final int btnSiete=0x7f080004;
+        public static final int btnTres=0x7f08000e;
+        public static final int btnUno=0x7f08000c;
         public static final int txvOperaciones=0x7f080001;
         public static final int txvResultado=0x7f080002;
     }
@@ -53,6 +58,23 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int lblClear=0x7f050013;
+        public static final int lblDivision=0x7f050012;
+        public static final int lblIgual=0x7f050011;
+        public static final int lblMas=0x7f050006;
+        public static final int lblMenos=0x7f05000a;
+        public static final int lblMultiplicacion=0x7f05000e;
+        public static final int lblNumCero=0x7f05000f;
+        public static final int lblNumCinco=0x7f050008;
+        public static final int lblNumCuatro=0x7f050007;
+        public static final int lblNumDos=0x7f05000c;
+        public static final int lblNumNueve=0x7f050005;
+        public static final int lblNumOcho=0x7f050004;
+        public static final int lblNumSeis=0x7f050009;
+        public static final int lblNumSiete=0x7f050003;
+        public static final int lblNumTres=0x7f05000d;
+        public static final int lblNumUno=0x7f05000b;
+        public static final int lblPunto=0x7f050010;
     }
     public static final class style {
         /** 
@@ -81,5 +103,8 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int botonNumero=0x7f060002;
+        public static final int operacionesTexto=0x7f060004;
+        public static final int resultadoNumero=0x7f060003;
     }
 }
--- SimpleCalculator/src/com/g3rcar/simplecalculator/MainActivity.java
+++ SimpleCalculator/src/com/g3rcar/simplecalculator/MainActivity.java
@@ -7,13 +7,18 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
 	Double resultados = 0d;
-	String tmpEscrito = "";
+	String tmpEscrito = "0";
 	String tmpChain = "";
+	boolean operando = false;
+	
 	final int SUMA = 1;
 	final int RESTA = 2;
+	final int MULTIPLICACION = 3;
+	final int DIVISION = 4;
 	final int IGUAL = 0;
 	
 	int operacion = SUMA;
@@ -24,237 +29,129 @@ public class MainActivity extends Activity implements OnClickListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main);			
 		
+		((Button)findViewById(R.id.btnCero)).setOnClickListener(this);
+		((Button)findViewById(R.id.btnUno)).setOnClickListener(this);
+		((Button)findViewById(R.id.btnDos)).setOnClickListener(this);
+		((Button)findViewById(R.id.btnTres)).setOnClickListener(this);
+		((Button)findViewById(R.id.btnCuatro)).setOnClickListener(this);
+		((Button)findViewById(R.id.btnCinco)).setOnClickListener(this);
+		((Button)findViewById(R.id.btnSeis)).setOnClickListener(this);
+		((Button)findViewById(R.id.btnSiete)).setOnClickListener(this);
+		((Button)findViewById(R.id.btnOcho)).setOnClickListener(this);
+		((Button)findViewById(R.id.btnNueve)).setOnClickListener(this);
+		((Button)findViewById(R.id.btnPunto)).setOnClickListener(this);
+		
+		((Button)findViewById(R.id.btnMas)).setOnClickListener(this);
+		((Button)findViewById(R.id.btnMenos)).setOnClickListener(this);
+		((Button)findViewById(R.id.btnMultiplicacion)).setOnClickListener(this);
+		((Button)findViewById(R.id.btnDivision)).setOnClickListener(this);
+		((Button)findViewById(R.id.btnIgual)).setOnClickListener(this);
+		((Button)findViewById(R.id.btnLimpiar)).setOnClickListener(this);
 		
 		txvOperaciones = (TextView)findViewById(R.id.txvOperaciones);
 		txvResultado = (TextView)findViewById(R.id.txvResultado);
-		
-		txvOperaciones.setText("");
-		txvResultado.setText("");
-		
-		/*
-		Button btnUno = (Button)findViewById(R.id.btnUno);
-		btnUno.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				tmpChain = txvOperaciones.getText().toString();
-				txvOperaciones.setText(tmpChain+"1");
-				tmpEscrito += "1";
-			}
-			
-		});
-		
-		Button btnDos = (Button)findViewById(R.id.btnDos);
-		btnDos.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				tmpChain = txvOperaciones.getText().toString();
-				txvOperaciones.setText(tmpChain+"2");
-				tmpEscrito += "2";
-			}
-			
-		});
-		
-		Button btnTres = (Button)findViewById(R.id.btnTres);
-		btnTres.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				tmpChain = txvOperaciones.getText().toString();
-				txvOperaciones.setText(tmpChain+"3");
-				tmpEscrito += "3";
-			}
-			
-		});
-		
-		Button btnCuatro = (Button)findViewById(R.id.btnCuatro);
-		btnCuatro.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				tmpChain = txvOperaciones.getText().toString();
-				txvOperaciones.setText(tmpChain+"4");
-				tmpEscrito += "4";
-			}
-			
-		});
-		
-		Button btnCinco = (Button)findViewById(R.id.btnCinco);
-		btnCinco.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				tmpChain = txvOperaciones.getText().toString();
-				txvOperaciones.setText(tmpChain+"5");
-				tmpEscrito += "5";
-			}
-			
-		});
-		
-		Button btnSeis = (Button)findViewById(R.id.btnSeis);
-		btnSeis.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				tmpChain = txvOperaciones.getText().toString();
-				txvOperaciones.setText(tmpChain+"6");
-				tmpEscrito += "6";
-			}
-			
-		});
-		
-		Button btnSiete = (Button)findViewById(R.id.btnSiete);
-		btnSiete.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				tmpChain = txvOperaciones.getText().toString();
-				txvOperaciones.setText(tmpChain+"7");
-				tmpEscrito += "7";
-			}
-			
-		});
-		
-		Button btnOcho = (Button)findViewById(R.id.btnOcho);
-		btnOcho.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				tmpChain = txvOperaciones.getText().toString();
-				txvOperaciones.setText(tmpChain+"8");
-				tmpEscrito += "8";
-			}
-			
-		});
-		
-		Button btnNueve = (Button)findViewById(R.id.btnNueve);
-		btnNueve.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				tmpChain = txvOperaciones.getText().toString();
-				txvOperaciones.setText(tmpChain+"9");
-				tmpEscrito += "9";
-			}
-			
-		});
-		
-		Button btnCero = (Button)findViewById(R.id.btnCero);
-		btnCero.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				tmpChain = txvOperaciones.getText().toString();
-				txvOperaciones.setText(tmpChain+"0");
-				tmpEscrito += "0";
-			}
-			
-		});
-		
-		Button btnMas = (Button)findViewById(R.id.btnMas);
-		btnMas.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				tmpChain = txvOperaciones.getText().toString();
-				txvOperaciones.setText(tmpChain+"+");
-				calcular(SUMA);
-				tmpEscrito = "";
-				txvResultado.setText(String.valueOf(resultados));
-				
-			}
-			
-		});
-		
-		Button btnMenos = (Button)findViewById(R.id.btnMenos);
-		btnMenos.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				tmpChain = txvOperaciones.getText().toString();
-				txvOperaciones.setText(tmpChain+"-");
-				calcular(RESTA);
-				tmpEscrito = "";
-				txvResultado.setText(String.valueOf(resultados));
-				
-			}
-			
-		});
-		
-		Button btnIgual = (Button)findViewById(R.id.btnIgual);
-		btnIgual.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				tmpChain = txvOperaciones.getText().toString();
-				txvOperaciones.setText("");
-				calcular(SUMA);
-				txvResultado.setText(String.valueOf(resultados));
-
-				resultados = 0d;
-				tmpEscrito = "";
-			}
-			
-		});
-		*/
-		
-	}
-	
-	public void calcular(int op){
-		switch(operacion){
-			case SUMA:
-				resultados = resultados + Double.parseDouble(tmpEscrito);
-				break;
-			case RESTA:
-				resultados = resultados - Double.parseDouble(tmpEscrito);
-				break;
-		}
-		
-		operacion = op;
 		
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
+	
+	
 	@Override
 	public void onClick(View view) {
-		tmpChain = txvOperaciones.getText().toString();
-		String tmpNum = "";
-		switch(view.getId()){
-			case R.id.btnCero: tmpNum="0"; break;
-			case R.id.btnUno: tmpNum="1"; break;
-			case R.id.btnDos: tmpNum="2"; break;
-			case R.id.btnTres: tmpNum="3"; break;
-			case R.id.btnCuatro: tmpNum="4"; break;
-			case R.id.btnCinco: tmpNum="5"; break;
-			case R.id.btnSeis: tmpNum="6"; break;
-			case R.id.btnSiete: tmpNum="7"; break;
-			case R.id.btnOcho: tmpNum="8"; break;
-			case R.id.btnNueve: tmpNum="9"; break;
-			//case R.id.btnPunto: tmpNum="."; break;
-			case R.id.btnMas:
-				tmpChain = txvOperaciones.getText().toString();
-				txvOperaciones.setText(tmpChain+"+");
-				calcular(SUMA);
-				tmpEscrito = "";
-				txvResultado.setText(String.valueOf(resultados));
-			break;
-		}
-		if(!tmpNum.equals("")){
-			txvOperaciones.setText(tmpChain+tmpNum);
-			tmpEscrito += tmpNum;
+		int id = view.getId();
+		String tmpNum="";
+		switch(id){
+		case R.id.btnCero: tmpNum = "0"; break;
+		case R.id.btnUno: tmpNum = "1"; break;
+		case R.id.btnDos: tmpNum = "2"; break;
+		case R.id.btnTres: tmpNum = "3"; break;
+		case R.id.btnCuatro: tmpNum = "4"; break;
+		case R.id.btnCinco: tmpNum = "5"; break;
+		case R.id.btnSeis: tmpNum = "6"; break;
+		case R.id.btnSiete: tmpNum = "7"; break;
+		case R.id.btnOcho: tmpNum = "8"; break;
+		case R.id.btnNueve: tmpNum = "9"; break;
+		case R.id.btnPunto: tmpNum = "."; break;
+		case R.id.btnMas: realizarOperacion(SUMA,"+"); break;
+		case R.id.btnMenos: realizarOperacion(RESTA,"-"); break;
+		case R.id.btnMultiplicacion: realizarOperacion(MULTIPLICACION,"x"); break;
+		case R.id.btnDivision: realizarOperacion(DIVISION,"/"); break;
+		case R.id.btnIgual: realizarOperacion(IGUAL,""); operando=false; break;
+		case R.id.btnLimpiar: resetTodo(); break;
 		}
 		
+		if(!tmpNum.equals("")){
+			operando=false;
+			if(tmpNum.equals(".")&&tmpEscrito.equals("")){
+				tmpChain += "0";
+				tmpEscrito = "0";
+			}
+			if(!(tmpNum.equals(".")&&tmpEscrito.contains(tmpNum))){
+				tmpChain += tmpNum;
+				tmpEscrito += tmpNum;
+				
+				txvOperaciones.setText(tmpChain);	
+			}
+		}
 	}
 	
+	public void realizarOperacion(int op,String t){
+		
+		if(!tmpEscrito.equals("") && !operando){
+			tmpChain+=t;
+			txvOperaciones.setText(tmpChain);
+			calcular(op);
+			tmpEscrito = "0";
+			txvResultado.setText(String.valueOf(resultados));
+			operando=true;
+		}
+	}
 	
+	private void calcular(int op){
+		try{
+			switch(operacion){
+				case SUMA:
+					resultados = resultados + Double.parseDouble(tmpEscrito);
+					break;
+				case RESTA:
+					resultados = resultados - Double.parseDouble(tmpEscrito);
+					break;
+				case MULTIPLICACION:
+					resultados = resultados * Double.parseDouble(tmpEscrito);
+					break;
+				case DIVISION:
+					if(tmpEscrito.equals("00")||tmpEscrito.equals("")){
+						throw new Exception("ERROR: División por 0");
+					}
+					resultados = resultados / Double.parseDouble(tmpEscrito);
+					break;
+				case IGUAL:
+					operando=false;
+					break;
+			}
+			
+			operacion = op;
+		}catch(Exception e){
+			Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+			resetTodo();
+		}
+	}
+	
+	private void resetTodo(){
+		resultados = 0d;
+		operacion = SUMA;
+		tmpEscrito = "";
+		tmpChain = "";
+		txvResultado.setText("0");
+		txvOperaciones.setText(tmpChain);
+		
+	}
 
 }
