c39742d2ae35f7d1ba2fa82bef189f4d2f89933d
Gerardo Calderón <estudianteudb-cep@iMac11.local>
2013-11-11 08:06:51 -0600

    Fin clase 15

--- Clase10Menu/src/com/udb/modulo1/clase10/MainActivity.java
+++ Clase10Menu/src/com/udb/modulo1/clase10/MainActivity.java
@@ -2,21 +2,35 @@ package com.udb.modulo1.clase10;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
-
+	String[] foods;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+
+		ListView listFoods = (ListView) findViewById(R.id.lstFood);
+		foods = getResources().getStringArray(R.array.food);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.fooditem, foods);
+		listFoods.setAdapter(adapter);
+		listFoods.setOnCreateContextMenuListener(this);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
@@ -41,6 +55,33 @@ public class MainActivity extends ActionBarActivity {
 			return true;
 		}
 	}
+	
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+	    super.onCreateContextMenu(menu, v, menuInfo);
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.context_menu, menu);
+	}
+
+	@Override
+		public boolean onContextItemSelected(MenuItem item) {
+			AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		    switch (item.getItemId()) {
+		        case R.id.iteminfo:
+		            Toast.makeText(this, "Informacion: "+foods[(int) info.id], Toast.LENGTH_SHORT).show();
+		            return true;
+		        case R.id.itemselecc:
+		        	Toast.makeText(this, "Selecciono: "+foods[(int) info.id], Toast.LENGTH_SHORT).show();
+		            return true;
+		        default:
+		            return super.onContextItemSelected(item);
+		    }
+		}
+	
+	
+	
+	
 	
 	
 
