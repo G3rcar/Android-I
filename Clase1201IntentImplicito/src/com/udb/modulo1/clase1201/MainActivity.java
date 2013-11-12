82c0403023f7257e1784a5cd9fdc8e7acd55d6d3
Gerardo Calderón <estudianteudb-cep@iMac11.local>
2013-11-12 08:07:26 -0600

    Fin clase 16

---
+++ Clase1201IntentImplicito/src/com/udb/modulo1/clase1201/MainActivity.java
@@ -0,0 +1,30 @@
+package com.udb.modulo1.clase1201;
+
+import com.udb.modulo1.clase12.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void callActivity(View view){
+		Intent intent = new Intent("ACTIVIDAD2");
+		startActivity(intent);
+	}
+}
